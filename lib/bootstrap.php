c9fd92e5f2164143129e4403611a633cc312e808
Peter Mescalchin <peter@magnetikonline.com.au>
2013-12-31 14:41:51 +1100

    Updated class autoloader to use the default spl_autoload()

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,24 +1,5 @@
 <?php
-class ClassLoader {
-
-	public static function load($nameSpec) {
-
-		require(static::getClassPath($nameSpec));
-	}
-
-	public static function exists($nameSpec) {
-
-		return is_file(static::getClassPath($nameSpec));
-	}
-
-	private static function getClassPath($nameSpec) {
-
-		return __DIR__ . '/' . strtr(strtolower($nameSpec),'\\','/') . '.php';
-	}
-}
-
-
-function __autoload($nameSpec) {
-
-	ClassLoader::load($nameSpec);
-}
+// setup class auto loader
+set_include_path(__DIR__);
+spl_autoload_extensions('.php');
+spl_autoload_register();
